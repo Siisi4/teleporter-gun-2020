2c0419430f47d6082a57fa75cc2f040b022e99b8
Siisi4 <siisi4officiel@gmail.com>
2020-06-26 12:19:50 +0200

    La base avec un fusil à projectile

---
+++ Teleporter-Gun-2020/Assets/#Project/Scripts/FireGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform spawnPoint;
+
+    private void Fire()
+    {
+        Vector3 position = spawnPoint.position;
+        Quaternion rotation = spawnPoint.rotation;
+
+        Instantiate(projectile, position, rotation);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+}
---
+++ Teleporter-Gun-2020/Assets/#Project/Scripts/StartImpulse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class StartImpulse : MonoBehaviour
+{
+
+    public float force = 1000f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        //multiplier le vecteur de l'avant (qui traverse l'objet pas le monde) par une force donc un nombre
+        // le vecteur de base commence à 1 (1 unité vers l'avant)
+        // donc on l'allonge en le multipliant. (ici par 1000)
+        rb.AddForce(transform.forward * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
