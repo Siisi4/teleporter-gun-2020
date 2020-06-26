1ff8af0765a8e5008e9ab3f0bdc0dbd93c22d235
Siisi4 <siisi4officiel@gmail.com>
2020-06-26 14:48:22 +0200

    Level 0 TUTORIAL _ platformes et recommencer si on tombe

---
+++ Teleporter-Gun-2020/Assets/#Project/Scripts/RescuePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescuePlayer : MonoBehaviour
+{
+
+    private GameObject player;
+    private Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        originalPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            CharacterController cc = player.GetComponent<CharacterController>();
+
+            cc.enabled = false;
+            player.transform.position = originalPosition;
+            cc.enabled = true;
+        }
+    }
+
+}
