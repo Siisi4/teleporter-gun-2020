5d1efa969401f75d8630a440fb432ce6868bd6d4
Siisi4 <siisi4officiel@gmail.com>
2020-09-07 12:21:58 +0200

    laser

---
+++ Laser/Assets/Scripts/hitmonster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitmonster : MonoBehaviour
+{
+    [Tooltip("Raycast range")]
+    public float r = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Color color = Color.blue;
+            if (hit.transform.tag == "Monster")
+            {
+                color = Color.magenta;
+                Debug.Log("Il y a un monstre ici");
+            }
+            Debug.DrawRay(transform.position, transform.forward * r, color);
+        }
+    }
+}
+
---
+++ Laser/Assets/Scripts/laser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    // notes pour moi //
+    [Tooltip("Raycast range")]
+    public float r = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fwd = transform.forward;
+
+
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, fwd * r, Color.red) ;
+        if (Physics.Raycast(transform.position, fwd, out hit))
+        {  
+            Debug.DrawRay(transform.position,fwd * r, Color.green);
+            Debug.Log("Tu touches quelque chose à " + hit.distance);
+        }
+        
+    }
+}
